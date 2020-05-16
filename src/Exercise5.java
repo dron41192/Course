3f1aa0f2b8d01af34737e264d3cfdd17c8799351
Andrey Rybalchenko <dron41192@github.com>
2020-05-17 00:11:34 +0300

    add new exercise

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class Exercise5 {
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
-        Scanner b = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Переменные должны быть больше 10 и меньше 1");
 
         System.out.print("Введите переменную а: ");
-        int s = a.nextInt();
-        if (s>10 || s<1) {
+        int s = scan.nextInt();
+        if (s > 10 || s < 1) {
             System.out.println("Вы ввели значение которое выходит за рамки условия задачи!");
             System.exit(0);
         }
-        System.out.print("Введите переменную b: ");
-        int d = b.nextInt();
-        if (d>10 || d<1) {
-            System.out.println("Вы ввели значение которое выходит за рамки условия задачи!");
-            System.exit(0);
-        }
-        int res=s*d;
 
-        System.out.print("Результат a*b равен: "+res);
+        out.println("Результат a*1 равен: " + s);
+        out.println("Результат a*2 равен: " + s * 2);
+        out.println("Результат a*3 равен: " + s * 3);
+        out.println("Результат a*4 равен: " + s * 4);
+        out.println("Результат a*5 равен: " + s * 5);
+        out.println("Результат a*6 равен: " + s * 6);
+        out.println("Результат a*7 равен: " + s * 7);
+        out.println("Результат a*8 равен: " + s * 8);
+        out.println("Результат a*9 равен: " + s * 9);
+        out.println("Результат a*10 равен: " + s * 10);
     }
 }
--- src/Exercise6.java
+++ src/Exercise6.java
@@ -2,17 +2,15 @@ import java.util.Scanner;
 
 public class Exercise6 {
     public static void main(String[] args) {
-        Scanner X = new Scanner(System.in);
-        Scanner Y = new Scanner(System.in);
-        Scanner Z = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("Введите x: ");
-        double x = X.nextInt();
+        int x = scan.nextInt();
         System.out.print("Введите y: ");
-        double y = Y.nextInt();
+        int y = scan.nextInt();
         System.out.print("Введите z: ");
-        double z = Z.nextInt();
-        double average = (x+y+z)/3;
+        int z = scan.nextInt();
+        double average = ((double)x+y+z)/3;
         System.out.printf("Среднее арифметическое = %.1f \n" , average);
         int result = (int) average/2;
         System.out.println(result);
